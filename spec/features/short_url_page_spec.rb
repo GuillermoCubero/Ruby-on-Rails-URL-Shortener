045ac04a5badc0ca6c46b6e97d5ee6da07f21089
Guille <guille22ce@gmail.com>
2017-04-29 22:35:46 +0000

    Añadiendo test de la Iteración 2

--- spec/features/short_url_page_spec.rb
+++ spec/features/short_url_page_spec.rb
@@ -1,10 +1,25 @@
 require "rails_helper"
 
 RSpec.feature "RSpec Test", :type => :feature do
-  scenario "Visit the Short Url Page and short an URL" do
-    visit "/greetings/short"  #en esta línea sobra el /greetings ??
+  
+  scenario "Visit the Short Url Page" do
+    visit "/short"
     expect(page).to have_text("Acortador de URL")
-    click_button "Acortar"
-    expect(page).to have_text("Tu URL acortada")
+    expect(page).to have_title("")#Titulo de la página? o con el have_text vale?
   end
+  
+  scenario "Short an invalid URL" do
+    visit "/short"
+    click_button "Acortar"
+    expect(page).to have_content("Error: Introduzca una URL válida")
+  end
+  
+  scenario "Short a valid URL" do
+    visit "/short"
+    fill_in "URL", with: "" ## una url válida. 
+    click_button "Acortar"
+    expect(page).to have_title("Tu URL acortada")
+    expect(page).to have_text("Copiar al portapapeles")
+  end
+  
 end
