2aefac9785175aca41c99fcb4d56ff3da9f8d1ba
Guille <guille22ce@gmail.com>
2017-04-29 19:03:22 +0000

    Añadido el primer test de la Iteración 2

--- spec/features/short_url_page_spec.rb
+++ spec/features/short_url_page_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "RSpec Test", :type => :feature do
   scenario "Visit the Short Url Page and short an URL" do
-    visit "/greetings/short"
+    visit "/greetings/short"  #en esta línea sobra el /greetings ??
     expect(page).to have_text("Acortador de URL")
     click_button "Acortar"
     expect(page).to have_text("Tu URL acortada")
