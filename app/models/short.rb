fe3050aff064416158f6fdfcc7a79180b831bda7
Guille <guille22ce@gmail.com>
2017-05-19 00:45:10 +0000

    Mejorados los test de modelo

--- app/models/short.rb
+++ app/models/short.rb
@@ -1,5 +1,5 @@
 class Short < ApplicationRecord
-    VALID_URL_REGEX = /(https?:\/\/)?(www\.)?\b[\S]+\.[\S]+/
+    VALID_URL_REGEX = /\A((http|https|ftp|ftps):\/\/)?(([a-z0-9]+\:)?[a-z0-9]+\@)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z/
     validates :url, presence: true, format: { with: VALID_URL_REGEX }
 
     def short_url
--- spec/models/short_spec.rb
+++ spec/models/short_spec.rb
@@ -2,26 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Short, type: :model do
     
-    let(:google) {Short.create(id: 123456789, url: 'https://www.google.es')}
-    let(:blank) {Short.create(id: 1, url: nil)}
-    let(:invalid) {Short.create(id: 2, url: 'http://www.google.')}
+    let(:short) { Short.create(id: 1234567890, url: 'http://www.google.es') }
+    let(:valids) { %w[https://www.google.es http://google.es https://google.es www.google.es google.es] }
+    let(:invalids) { %w[http://www.google. https://.google.es http://google. https://google http://.] }
+    
+    subject { short }
+     
+    it { should respond_to(:id) }
+    it { should respond_to(:url) }
+    it { should respond_to(:short_url) }
+    
+    it { should be_valid }
     
     context 'Short an URL' do
         
         it 'has to translate the id to b36' do
-            expect(google.short_url).to eq google.id.to_s(36)
+            expect(short.short_url).to eq short.id.to_s(36)
         end
         
-        it 'has to be a valid url' do
-            expect(google).to be_valid
+        it 'has to percive when url is not present' do
+            short.url = nil
+            expect(short).not_to be_valid
         end
         
-        it 'has to be a blank invalid url' do
-            expect(blank).to_not be_valid
+        it 'has to percibe when url is valid' do
+            valids.each do |valid_url|
+                short.url = valid_url
+                expect(short).to be_valid
+            end
         end
         
-        it 'has to be an ivalid url' do
-            expect(invalid).to_not be_valid
+        it 'has to percibe when url is invalid' do
+            invalids.each do |invalid_url|
+                short.url = invalid_url
+                expect(short).not_to be_valid
+            end
         end
 
     end
