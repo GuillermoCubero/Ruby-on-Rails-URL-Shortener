cf0736b08fa717677f984e4be525ebb97ad13ba4
Guille <guille22ce@gmail.com>
2017-05-24 12:57:15 +0000

    Cambios en la db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504111425) do
+ActiveRecord::Schema.define(version: 20170524124103) do
 
   create_table "shorts", force: :cascade do |t|
     t.string   "url"
