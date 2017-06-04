513831fef906fe4f31d8d0d76c02b913c5c55328
Guille <guille22ce@gmail.com>
2017-06-04 16:53:32 +0000

    Cambios finales de la Iteración 2

--- spec/models/short_spec.rb
+++ spec/models/short_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe Short, type: :model do
     end
     
     context 'Short a valid URL' do
-        %w[https://www.google.es http://google.es https://google.es www.google.es].each do |valid_url|
+        %w[https://www.google.es http://google.es https://google.es www.google.es google.es].each do |valid_url|
             it 'has to short a valid url' do
                 short.url = valid_url
                 expect(short).to be_valid
