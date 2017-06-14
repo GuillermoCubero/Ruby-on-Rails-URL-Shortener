fda00cfdeeeb0b32615820ec14c01bdbf873ee53
Guille <guille22ce@gmail.com>
2017-06-14 13:05:55 +0000

    Añadido gestión de cuentas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,17 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "guille22ce@gmail.com",
+    password: "yugioh"
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "short_url_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  
+  config.action_mailer.default_url_options = { host: 'http://acortador-urls.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "guille22ce@gmail.com",
+    password: "yugioh" 
+  }
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
