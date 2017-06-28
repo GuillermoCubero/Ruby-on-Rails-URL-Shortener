070570e32ff3b0e78e4a3dd60ceb2f84eed9e432
Guillermo Cubero <guille22ce@gmail.com>
2017-06-28 19:29:29 +0000

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root 'static_pages#home'
-  get 'greetings/hello'
-  devise_for :users
-  resources :user, only: [:index, :destroy, :show, :update]
-  resources :shorts
-  resources :advertisements
-  get 'static_pages/manageadvertisements'
-=======
 
   namespace :admin do
     resources :shorts, only: [:index, :edit, :update, :destroy]
@@ -18,8 +9,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   resources :shorts, only: [:show, :create, :new]
+  resources :advertisements
   get 'greetings/hello'
->>>>>>> master
   get ":id", to: "shorts#redirect_id"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
