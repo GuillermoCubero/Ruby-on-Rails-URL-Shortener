db6df7fa1360ba82368a1f97e27aa1a11868e73a
Guillermo Cubero <guille22ce@gmail.com>
2017-07-08 18:29:01 +0000

    Añadida comprobacion del redirect

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,9 @@ if Rails.env.production?
       :provider              => 'AWS',
       :region => ENV['S3_REGION'],
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :path_style            => true
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory =  ENV['S3_BUCKET']
   end
 end
