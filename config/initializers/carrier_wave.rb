ee00c6153d53734b2c67d3efdd1448d3f78b5d1c
Guillermo Cubero <guille22ce@gmail.com>
2017-07-08 19:16:49 +0000

    S3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,10 +6,8 @@ if Rails.env.production?
       :region => ENV['S3_REGION'],
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :path_style            => true
     }
     config.fog_directory =  ENV['S3_BUCKET']
     config.cache_dir = "#{Rails.root}/tmp/uploads"
-    config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET']}"
   end
 end
