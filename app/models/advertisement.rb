62a9e93cc7fe87243e2de70309f7cf31186efdd2
Guillermo Cubero <guille22ce@gmail.com>
2017-07-09 01:01:21 +0000

    Cambios iteracion 6

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -15,8 +15,10 @@ class Advertisement < ApplicationRecord
   end
   
   def picture_size
-    if picture.size > 5.megabytes
-      errors.add(:picture, "should be less than 5MB.")
+    unless picture.blank?
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB.")
+      end
     end
   end
 
